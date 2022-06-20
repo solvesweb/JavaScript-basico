57cb617ee76411b39119d46a78aceb025405e7fc
Solves <90395613+solvesweb@users.noreply.github.com>
2022-06-20 12:40:06 +0200

    Add files via upload

---
+++ 10-Modulos-en-JS/ejercicio-13/index.js
@@ -0,0 +1,8 @@
+import * as moduloMates from './controller.js';
+import chalk from 'chalk';
+
+const suma1 = moduloMates.suma(1, 2)
+console.log(suma1);
+
+const suma2 = moduloMates.suma(4, 5)
+console.log(chalk.green(suma2));
